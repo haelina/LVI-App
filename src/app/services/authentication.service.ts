e39c44fe258fd6a8d1adde21f1d81d5b4b806551
haelina <56834622+haelina@users.noreply.github.com>
2022-02-28 09:02:21 +0200

    Add required functions for auth service

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from '@angular/fire/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
+  constructor(private auth: Auth) {}
 
-  constructor() { }
+  async register({ email, password }) {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        this.auth,
+        email,
+        password
+      );
+      return user;
+    } catch (err) {
+      return null;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      const user = signInWithEmailAndPassword(this.auth, email, password);
+      return user;
+    } catch (err) {
+      return null;
+    }
+  }
+
+  logout() {
+    return signOut(this.auth);
+  }
 }
