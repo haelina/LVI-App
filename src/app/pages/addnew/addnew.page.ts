49033d6ae825b95d2ee1e7967c348418fd51ada5
haelina <56834622+haelina@users.noreply.github.com>
2022-03-01 17:26:34 +0200

    Add datepicker related function and variables

--- src/app/pages/addnew/addnew.page.ts
+++ src/app/pages/addnew/addnew.page.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonDatetime } from '@ionic/angular';
+import { format, parseISO } from 'date-fns';
+import { IonRouterOutlet } from '@ionic/angular';
 @Component({
   selector: 'app-addnew',
   templateUrl: './addnew.page.html',
   styleUrls: ['./addnew.page.scss'],
 })
 export class AddnewPage implements OnInit {
+  @ViewChild(IonDatetime, { static: true }) datetime: IonDatetime;
 
-  constructor() { }
+  dateValue = '';
+  dateFormatted = '';
+  datepickerVisible = false;
 
-  ngOnInit() {
+  constructor(public routerOutlet: IonRouterOutlet) {}
+
+  ngOnInit() {}
+
+  formatDate(value: string) {
+    return format(parseISO(value), 'dd.MM.yyyy');
   }
 
+  handleDatepick(datePicked) {
+    this.dateValue = datePicked;
+    this.dateFormatted = this.formatDate(datePicked);
+    this.datepickerVisible = false;
+  }
 }
