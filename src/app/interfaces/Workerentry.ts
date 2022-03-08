93a0b873630bb7ab83022d08f2f960597d69abda
haelina <56834622+haelina@users.noreply.github.com>
2022-03-08 10:42:22 +0200

    Add interface for new worker entry

---
+++ src/app/interfaces/Workerentry.ts
@@ -0,0 +1,11 @@
+interface Workerentry {
+  uid?: string;
+  locations: string[];
+  date: string;
+  startTime: string;
+  endTime: string;
+  details: string;
+  isTrainee: boolean;
+}
+
+export default Workerentry;
