1cc23a488fea07d8ada53ced0d8274869832225d
haelina <56834622+haelina@users.noreply.github.com>
2022-03-08 12:50:28 +0200

    Add method for getting all worker entries

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { collection, addDoc } from '@firebase/firestore';
 import { AuthenticationService } from './authentication.service';
-import { Firestore } from '@angular/fire/firestore';
+import { collectionData, Firestore } from '@angular/fire/firestore';
 import WorkerEntry from '../interfaces/Workerentry';
 import JobEntry from '../interfaces/JobEntry';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -20,5 +21,10 @@ export class FirestoreService {
 
   async addJob(job: JobEntry) {
     await addDoc(collection(this.firestore, 'jobs'), job);
+  }
+
+  getWorkers(): Observable<WorkerEntry[]> {
+    const workerRef = collection(this.firestore, 'workers');
+    return collectionData(workerRef) as Observable<WorkerEntry[]>;
   }
 }
