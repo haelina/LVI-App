59581e1eeedd2e7088a9fce720a34c11e57bdc31
haelina <56834622+haelina@users.noreply.github.com>
2022-03-08 17:07:25 +0200

    Add workerdata variable and use firestoreservice

--- src/app/pages/trainees/trainees.page.ts
+++ src/app/pages/trainees/trainees.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import WorkerEntry from 'src/app/interfaces/Workerentry';
+import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Component({
   selector: 'app-trainees',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trainees.page.scss'],
 })
 export class TraineesPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  workerData: WorkerEntry[];
+  constructor(private firestore: FirestoreService) {
+    this.firestore.getWorkers(true).subscribe((res) => {
+      this.workerData = res;
+      console.log(this.workerData);
+    });
   }
 
+  ngOnInit() {}
 }
