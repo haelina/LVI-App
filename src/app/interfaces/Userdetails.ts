3865f8ed17cbc60d8c09e9be2b9a178a302a7a37
haelina <56834622+haelina@users.noreply.github.com>
2022-03-08 10:38:33 +0200

    Add interface for further userdetails

---
+++ src/app/interfaces/Userdetails.ts
@@ -0,0 +1,13 @@
+interface Userdetails {
+  uid?: number;
+  firstName: string;
+  lastName: string;
+  company: string;
+  streetAddress: string;
+  zip: string;
+  city: string;
+  phone: string;
+  email: string;
+}
+
+export default Userdetails;
