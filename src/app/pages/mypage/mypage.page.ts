9d41d692e01c43a7c86d8c6b80c2a42c4d88b14f
haelina <56834622+haelina@users.noreply.github.com>
2022-03-09 15:25:54 +0200

    Add formgroup and some validation for userdetails

--- src/app/pages/mypage/mypage.page.ts
+++ src/app/pages/mypage/mypage.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mypage',
@@ -8,10 +10,40 @@ import { Router } from '@angular/router';
   styleUrls: ['./mypage.page.scss'],
 })
 export class MypagePage implements OnInit {
-  constructor(private auth: Auth, private router: Router) {
-    console.log(auth.currentUser);
-    console.log(this.router.url);
+  myData: FormGroup;
+
+  constructor(
+    private auth: Auth,
+    private router: Router,
+    private formbuilder: FormBuilder,
+    private loadingController: LoadingController,
+    private alertController: AlertController
+  ) {
+    //console.log(auth.currentUser);
+    //console.log(this.router.url);
   }
 
-  ngOnInit() {}
+  async ngOnInit() {
+    //await getMyData();
+    this.myData = this.formbuilder.group({
+      company: ['Esimerkki Oy', [Validators.minLength(5)]],
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      address: ['', [Validators.minLength(5)]],
+      zip: ['', [Validators.maxLength(5), Validators.maxLength(5)]],
+      city: ['', [Validators.minLength(2)]],
+      phone: [
+        '',
+        [Validators.required],
+        Validators.minLength(5),
+        Validators.maxLength(20),
+      ],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  updateMyData() {
+    console.log('clicked submit');
+    console.log(this.myData.value);
+  }
 }
