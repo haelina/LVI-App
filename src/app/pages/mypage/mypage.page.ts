a150f64f03d0c9f11b5e31b90a945a5364378179
haelina <56834622+haelina@users.noreply.github.com>
2022-03-10 10:45:15 +0200

    Add function to insert data if userdetails not found from db

--- src/app/pages/mypage/mypage.page.ts
+++ src/app/pages/mypage/mypage.page.ts
@@ -15,13 +15,15 @@ export class MypagePage implements OnInit {
   myData: FormGroup;
   userdetails: Userdetails;
   isLoading = true;
+  newUser = true;
 
   constructor(
     private router: Router,
     private firestore: FirestoreService,
     private formbuilder: FormBuilder,
     private loadingController: LoadingController,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private auth: Auth
   ) {
     //console.log(auth.currentUser);
     //console.log(this.router.url);
@@ -58,6 +60,7 @@ export class MypagePage implements OnInit {
     return this.myData.get('email');
   }
 
+  /*
   async fetchUserdetail() {
     await this.firestore.getUserDetail().subscribe((res) => {
       this.userdetails = res;
@@ -94,14 +97,94 @@ export class MypagePage implements OnInit {
       this.isLoading = false;
     });
   }
+  */
+
+  initializeForm(userDetails: Userdetails | null) {
+    this.myData = this.formbuilder.group({
+      company: [
+        userDetails ? userDetails.company : '',
+        [Validators.minLength(5)],
+      ],
+      firstName: [
+        userDetails ? userDetails.firstName : '',
+        [Validators.required, Validators.minLength(2)],
+      ],
+      lastName: [
+        userDetails ? userDetails.lastName : '',
+        [Validators.required, Validators.minLength(2)],
+      ],
+      address: [
+        userDetails ? userDetails.address : '',
+        [Validators.minLength(5)],
+      ],
+      zip: [
+        userDetails ? userDetails.zip : '',
+        [Validators.minLength(5), Validators.maxLength(5)],
+      ],
+      city: [userDetails ? userDetails.city : '', [Validators.minLength(2)]],
+      phone: [
+        userDetails ? userDetails.phone : '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(20),
+        ],
+      ],
+      email: [
+        userDetails ? userDetails.email : this.auth.currentUser.email,
+        [Validators.required, Validators.email],
+      ],
+    });
+    this.isLoading = false;
+  }
+
+  async getUserDetails() {
+    const found = await this.firestore.getUser();
+    if (found) {
+      this.userdetails = found;
+      this.initializeForm(this.userdetails);
+      this.newUser = false;
+    } else {
+      this.initializeForm(null);
+      this.newUser = true;
+    }
+  }
 
   ngOnInit() {
-    this.fetchUserdetail();
+    this.getUserDetails();
+    /*
+    console.log(this.firestore.getUser());
+    if (this.newUser) {
+      this.myData = this.formbuilder.group({
+        company: ['', [Validators.minLength(5)]],
+        firstName: ['', [Validators.required, Validators.minLength(2)]],
+        lastName: ['', [Validators.required, Validators.minLength(2)]],
+        address: ['', [Validators.minLength(5)]],
+        zip: ['', [Validators.minLength(5), Validators.maxLength(5)]],
+        city: ['', [Validators.minLength(2)]],
+        phone: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.maxLength(20),
+          ],
+        ],
+        email: ['', [Validators.required, Validators.email]],
+      });
+    } else {
+      this.fetchUserdetail();
+    }
+
+    */
   }
 
   updateMyData() {
     //console.log('clicked submit');
-    //console.log(this.myData.value);
+
     this.firestore.addUserdetail(this.myData.value);
+    if (this.newUser) {
+      this.newUser = false;
+    }
   }
 }
