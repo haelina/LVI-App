e563a035ce47a0245d3a3544887f834c85805e28
haelina <56834622+haelina@users.noreply.github.com>
2022-03-17 10:33:49 +0200

    Import modify-job module

--- src/app/components/my-job-card/my-job-card.module.ts
+++ src/app/components/my-job-card/my-job-card.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyJobCardComponent } from './my-job-card.component';
+import { ModifyJobPageModule } from 'src/app/pages/modify-job/modify-job.module';
 
 @NgModule({
   declarations: [MyJobCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ModifyJobPageModule],
   exports: [MyJobCardComponent],
 })
 export class MyJobCardModule {}
