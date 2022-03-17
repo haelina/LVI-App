56e181d9fde50d51ecd9317a4291270c21a03cae
haelina <56834622+haelina@users.noreply.github.com>
2022-03-17 13:20:43 +0200

    Create form and fill values

--- src/app/pages/modify-job/modify-job.page.ts
+++ src/app/pages/modify-job/modify-job.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { ModalController } from '@ionic/angular';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalController, ToastController } from '@ionic/angular';
+import JobEntry from 'src/app/interfaces/JobEntry';
+import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Component({
   selector: 'app-modify-job',
@@ -8,13 +10,53 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./modify-job.page.scss'],
 })
 export class ModifyJobPage implements OnInit {
+  @Input() job: JobEntry;
   modifiedData: FormGroup;
+  formLoading = true;
 
-  constructor(private modalController: ModalController) {}
+  constructor(
+    private modalController: ModalController,
+    private firestore: FirestoreService,
+    private formbuilder: FormBuilder,
+    private toastController: ToastController
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initializeJobForm(this.job);
+  }
+
+  initializeJobForm(job: JobEntry) {
+    this.modifiedData = this.formbuilder.group({
+      uid: [job.uid],
+      locations: [job.locations],
+      timing: [job.timing, [Validators.required, Validators.minLength(2)]],
+      address: [job.address, [Validators.minLength(2)]],
+      duration: [job.duration, [Validators.minLength(2)]],
+      details: [job.details, [Validators.required, Validators.minLength(2)]],
+    });
+    this.formLoading = false;
+  }
 
   closeModal() {
     this.modalController.dismiss();
+  }
+
+  sendModifiedJob() {
+    console.log('sending data');
+    this.closeModal();
+    this.presentToastWithMessage(
+      'Onnea tekijän löytämiseen. Ilmoituksesi on päivitetty Työdiileriin.'
+    );
+  }
+
+  async presentToastWithMessage(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      color: 'danger',
+      icon: 'sparkles',
+      position: 'middle',
+      duration: 3000,
+    });
+    await toast.present();
   }
 }
