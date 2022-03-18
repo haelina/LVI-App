af2deb88be5e1b4b19a77ed6f875212652b68c4a
haelina <56834622+haelina@users.noreply.github.com>
2022-03-18 16:43:45 +0200

    Filter workers by date range

--- src/app/pages/workforce/workforce.page.ts
+++ src/app/pages/workforce/workforce.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarComponentOptions } from 'ion2-calendar';
+import * as moment from 'moment';
 import WorkerEntry from 'src/app/interfaces/Workerentry';
 import { FirestoreService } from 'src/app/services/firestore.service';
 
@@ -11,6 +12,18 @@ import { FirestoreService } from 'src/app/services/firestore.service';
 export class WorkforcePage implements OnInit {
   workerData: WorkerEntry[];
   filterLocations: string[];
+
+  dateRange: { from: string; to: string };
+  type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
+  optionsRange: CalendarComponentOptions = {
+    pickMode: 'range',
+    weekStart: 1,
+    monthFormat: 'MMMM YYYY',
+    showMonthPicker: false,
+    weekdays: ['SU', 'MA', 'TI', 'KE', 'TO', 'PE', 'LA'],
+    color: 'primary',
+  };
+
   options: CalendarComponentOptions = {
     pickMode: 'multi',
     color: 'danger',
@@ -28,12 +41,25 @@ export class WorkforcePage implements OnInit {
 
   getFiltered() {
     console.log('Filtered by locations: ' + this.filterLocations);
+    //const start = '2022-03-25T00:00:00+02:00';
+    //const end = '2022-03-31T00:00:00+02:00';
     this.firestore
       .filterWorkersByLocation(this.filterLocations)
       .subscribe((res) => {
-        this.workerData = res;
+        const start = moment(this.dateRange.from).format();
+        const end = moment(this.dateRange.to).format();
+        const filteredWorkers = [];
+        res.forEach((w) => {
+          w.dates.forEach((d) => {
+            if (start.localeCompare(d) <= 0 && end.localeCompare(d) >= 0) {
+              filteredWorkers.push(w);
+            }
+          });
+        });
+        this.workerData = filteredWorkers;
         console.log(this.workerData);
       });
+    //console.log(moment(this.dateRange.from).format());
   }
 
   getOptions(dates): CalendarComponentOptions {
