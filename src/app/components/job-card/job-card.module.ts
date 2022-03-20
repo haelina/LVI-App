c73ef84c90fc1ceb7b2008236578a0e6e41d7f20
haelina <56834622+haelina@users.noreply.github.com>
2022-03-20 11:04:37 +0200

    Export JobCardComponent in module

--- src/app/components/job-card/job-card.module.ts
+++ src/app/components/job-card/job-card.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { JobCardComponent } from './job-card.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [JobCardComponent],
+  imports: [CommonModule],
+  exports: [JobCardComponent],
 })
-export class JobCardModule { }
+export class JobCardModule {}
