f9498d04e73c86ae00860d4d0a169766b9115b4b
haelina <56834622+haelina@users.noreply.github.com>
2022-03-20 10:52:34 +0200

    Add component for showing job in customer view

---
+++ src/app/components/job-card/job-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-card',
+  templateUrl: './job-card.component.html',
+  styleUrls: ['./job-card.component.scss'],
+})
+export class JobCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
