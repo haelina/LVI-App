dc508c6311b80759150b1b4e40067385063e89d6
haelina <56834622+haelina@users.noreply.github.com>
2022-03-20 12:59:35 +0200

    Add component for trainee-card showing in trainees page

---
+++ src/app/components/trainee-card/trainee-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trainee-card',
+  templateUrl: './trainee-card.component.html',
+  styleUrls: ['./trainee-card.component.scss'],
+})
+export class TraineeCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/components/trainee-card/trainee-card.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TraineeCardComponent } from './trainee-card.component';
+
+@NgModule({
+  declarations: [TraineeCardComponent],
+  imports: [CommonModule],
+  exports: [TraineeCardComponent],
+})
+export class TraineeCardModule {}
