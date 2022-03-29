0751491fbda51014e860feb0bca02fccd31021a9
haelina <56834622+haelina@users.noreply.github.com>
2022-03-29 16:53:55 +0300

    Add CallNumber to providers

--- src/app/components/job-card/job-card.module.ts
+++ src/app/components/job-card/job-card.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JobCardComponent } from './job-card.component';
+import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
 
 @NgModule({
   declarations: [JobCardComponent],
   imports: [CommonModule],
+  providers: [CallNumber],
   exports: [JobCardComponent],
 })
 export class JobCardModule {}
