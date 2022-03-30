21a7b016eb9c7eccb903217fedafeb4257f83387
haelina <56834622+haelina@users.noreply.github.com>
2022-03-30 15:13:18 +0300

    Add provider for callnumber component

--- src/app/components/trainee-card/trainee-card.module.ts
+++ src/app/components/trainee-card/trainee-card.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TraineeCardComponent } from './trainee-card.component';
+import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
 
 @NgModule({
   declarations: [TraineeCardComponent],
   imports: [CommonModule],
   exports: [TraineeCardComponent],
+  providers: [CallNumber],
 })
 export class TraineeCardModule {}
