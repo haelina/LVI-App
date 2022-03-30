9009a57830a51547aedad9d58131d353914354e8
haelina <56834622+haelina@users.noreply.github.com>
2022-03-30 08:51:18 +0300

    Import calendarmodule for ion2-calendar

--- src/app/components/workercard/workercard.module.ts
+++ src/app/components/workercard/workercard.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkercardComponent } from './workercard.component';
 import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
+import { CalendarModule } from 'ion2-calendar';
 
 @NgModule({
   declarations: [WorkercardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, CalendarModule],
   exports: [WorkercardComponent],
   providers: [CallNumber],
 })
