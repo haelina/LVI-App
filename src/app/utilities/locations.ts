7ceb4b86f484a305fdacbfa8efa59e325a12f254
haelina <56834622+haelina@users.noreply.github.com>
2022-03-30 10:41:05 +0300

    Add utility function that contains list of all locations

---
+++ src/app/utilities/locations.ts
@@ -0,0 +1,27 @@
+const getLocationList = (): string[] => [
+  'Akaa',
+  'Hämeenkyrö',
+  'Ikaalinen',
+  'Juupajoki',
+  'Kangasala',
+  'Kihniö',
+  'Kuhmoinen',
+  'Lempäälä',
+  'Mänttä-Vilppula',
+  'Nokia',
+  'Orivesi',
+  'Parkano',
+  'Pirkkala',
+  'Punkalaidun',
+  'Pälkäne',
+  'Ruovesi',
+  'Sastamala',
+  'Tampere',
+  'Urjala',
+  'Valkeakoski',
+  'Virrat',
+  'Vesilahti',
+  'Ylöjärvi',
+];
+
+export { getLocationList };
