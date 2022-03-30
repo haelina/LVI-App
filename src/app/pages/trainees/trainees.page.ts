1f218df1b174406992d376f9b1c5abc034057e28
haelina <56834622+haelina@users.noreply.github.com>
2022-03-30 14:48:51 +0300

    Change bg color and refactor ion-select-options with ngFor

--- src/app/pages/trainees/trainees.page.ts
+++ src/app/pages/trainees/trainees.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import TraineeEntry from 'src/app/interfaces/TraineeEntry';
 import { FirestoreService } from 'src/app/services/firestore.service';
+import { getLocationList } from 'src/app/utilities/locations';
 
 @Component({
   selector: 'app-trainees',
@@ -8,6 +9,8 @@ import { FirestoreService } from 'src/app/services/firestore.service';
   styleUrls: ['./trainees.page.scss'],
 })
 export class TraineesPage implements OnInit {
+  locationList = getLocationList();
+  showFilters = false;
   traineeData: TraineeEntry[];
   filterLocations: string[];
 
@@ -28,5 +31,6 @@ export class TraineesPage implements OnInit {
         this.traineeData = res;
         console.log(this.traineeData);
       });
+    this.showFilters = false;
   }
 }
